09321b6db6316c460ce7c23482ddce6eeb660b03
Nicklas Bring Nielsen <nicklas.bring.nielsen@gmail.com>
2019-09-23 22:56:05 +0200

    exercise 2 done

--- js/exercises.js
+++ js/exercises.js
@@ -1,11 +1,30 @@
 //Exercise 1 
-console.log('#')
-console.log('##')
-console.log('###')
-console.log('####')
-console.log('#####')
-console.log('######')
-console.log('#######')
+console.log('This is exercise 1');
+console.log('#');
+console.log('##');
+console.log('###');
+console.log('####');
+console.log('#####');
+console.log('######');
+console.log('#######');
 
 
 //Exercise 2
+console.log('');
+console.log('This is exercise 2');
+for(i = 0; i < 100; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log(i +': FizzBuzz');
+    }
+    else if(i % 3 === 0){
+        console.log(i + ': Fizz');
+    }
+    else if(i % 5 === 0){
+        console.log(i + ': Buzz');
+    }
+} 
+
+
+//Exercise 3
+console.log('');
+console.log('This is exercise 3');
